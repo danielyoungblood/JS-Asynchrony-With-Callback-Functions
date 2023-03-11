a2c6404673d827a4c4985fd492d0c52b23216ee6
daniel <danielyknechtli@gmail.com>
2023-03-10 16:57:44 -0800

    moving non playable character using callbacks

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,59 +1,86 @@
 function newNonPlayableCharacter(x, y) {
-    let element = newImage('assets/red-character/static.gif')
-    element.style.zIndex = 1;
+  let element = newImage("assets/red-character/static.gif");
+  element.style.zIndex = 1;
 
-    let direction = null;
+  let direction = null;
 
-    function moveCharacter() {
-        if (direction === 'west') {
-            x -= 1
-        }
-        if (direction === 'north') {
-            y += 1
-        }
-        if (direction === 'east') {
-            x += 1
-        }
-        if (direction === 'south') {
-            y -= 1
-        }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
+  function moveCharacter() {
+    if (direction === "west") {
+      x -= 1;
     }
-
-    setInterval(moveCharacter, 1)
-
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
+    if (direction === "north") {
+      y += 1;
     }
-
-    function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+    if (direction === "east") {
+      x += 1;
     }
-
-    function walkWest() {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
+    if (direction === "south") {
+      y -= 1;
     }
+    element.style.left = x + "px";
+    element.style.bottom = y + "px";
+  }
 
-    function walkSouth() {
-        direction = 'south'
-        element.src = `./assets/red-character/south.gif`
-    }
+  setInterval(moveCharacter, 1);
 
-    function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
-    }
+  // define a function named walkEast with 2 parameters:
+  //1) time - number in milliseconds
+  //2) callback - name of a function or function definition defined as () => {}
+  function walkEast(time, callback) {
+    direction = "east";
+    element.src = `./assets/green-character/east.gif`;
+    setTimeout(() => {
+      stop();
+      if (callback) {
+        callback();
+      }
+    }, time);
+  }
 
-    return {
-        element: element,
-        walkWest: walkWest,
-        walkNorth: walkNorth,
-        walkEast: walkEast,
-        walkSouth: walkSouth,
-        stop: stop
-    }
+  function walkNorth(time, callback) {
+    direction = "north";
+    element.src = `./assets/red-character/north.gif`;
+    setTimeout(() => {
+      stop();
+      if (callback) {
+        callback();
+      }
+    }, time);
+  }
+
+  function walkWest(time, callback) {
+    direction = "west";
+    element.src = `./assets/red-character/west.gif`;
+    setTimeout(() => {
+      stop();
+      if (callback) {
+        callback();
+      }
+    }, time);
+  }
+
+  function walkSouth(time, callback) {
+    direction = "south";
+    element.src = `./assets/red-character/south.gif`;
+    setTimeout(() => {
+      stop();
+      if (callback) {
+        callback();
+      }
+    }, time);
+  }
+
+  function stop() {
+    direction = null;
+    element.src = `./assets/red-character/static.gif`;
+  }
+
+  return {
+    element: element,
+    walkWest: walkWest,
+    walkNorth: walkNorth,
+    walkEast: walkEast,
+    walkSouth: walkSouth,
+    stop: stop,
+  };
 }
